ec0cdf77958997eb7bf2370c5c2af0b14486a023
NikC888 <isuzashi@gmail.com>
2016-03-24 19:21:23 -0700

    Clean SimpleGreeting.java more

--- src/SimpleGreeting.java
+++ src/SimpleGreeting.java
@@ -5,26 +5,19 @@ import java.util.Scanner;
 public class SimpleGreeting {
 	//create a class variable of a scanner object to allow user input to console
 	static Scanner scanner = new Scanner(System.in);
-	//static class variable to hold string for user name
-	//static String username;
 	
 	public static void main(String[] args) {
 		//declare local variables
-		String name;
-		String loop = "not exit";
+		String name, loop = "not exit";
 		//out a welcome message to user
 		System.out.println("Hello, and welcome to the simple greeting app\nPlease enter your name.");
 		//set the captured name to a local string variable, name
 		name = scanner.nextLine();
 		//output the information to the user
 		System.out.println("Well *" + name + "* thanks for sharing! \nPlease type \"exit\" to terminate the program");
-		//call method which gives a farewell to user and exits the system
 		loop = scanner.nextLine();
 		
 		//forever loop until user types "exit"
-		//examples of string comparing
-		//Objects.equals("test", new String("test")) // --> true
-		//Objects.equals(null, "test") // --> false
 		while (!loop.equals("exit")){
 			System.out.println("You did not type \"exit\", please try again");
 			loop = scanner.nextLine();
@@ -35,7 +28,5 @@ public class SimpleGreeting {
 		System.out.println("Thank you for using the simple greeting Application!");
 		//exit the system
 		System.exit(0);
-		//exitApplication();
 	}
-
 }
